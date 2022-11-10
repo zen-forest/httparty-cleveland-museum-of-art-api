2f7455bf0cb672c496aac2946e2c6ed87fa34f34
metasignal <timcollinsdesign+metasignal@gmail.com>
2022-11-10 12:57:07 -0500

    added nationality search, not very sophisticated here

--- app.rb
+++ app.rb
@@ -5,8 +5,16 @@ class Cma
   base_uri 'https://openaccess-api.clevelandart.org/api'
 
   def creators(artist)
-    @artist = artist
+    @artist = artist.to_s
     uri = "/creators/?name=#{artist}"
+    puts self.class.get(uri)
+    # puts self.class.get('/creators/?name=#{artist}')
+  end
+
+  def creator_nationality(nationality, limit)
+    @nationality = nationality.to_s
+    @limit = limit
+    uri = "/creators/?nationality=#{nationality}&limit=#{limit}"
     puts self.class.get(uri)
     # puts self.class.get('/creators/?name=#{artist}')
   end
@@ -14,6 +22,7 @@ class Cma
 end
 
 cleveland_museum_of_art = Cma.new
-puts cleveland_museum_of_art.creators("monet")
+cleveland_museum_of_art.creator_nationality("American", 4)
+# puts cleveland_museum_of_art.creators("monet")
 
 
