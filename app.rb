c53791ca09a250afeceb5980360482ab9b75c1c6
metasignal <timcollinsdesign+metasignal@gmail.com>
2022-11-10 12:37:50 -0500

    artist search is working

--- app.rb
+++ app.rb
@@ -1,32 +1,19 @@
 require 'httparty'
 
-response = HTTParty.get('https://openaccess-api.clevelandart.org/api/creators/?name=Monet')
-
-# puts response.body, response.code, response.message, response.headers.inspect
-
 class Cma
   include HTTParty
   base_uri 'https://openaccess-api.clevelandart.org/api'
-  
-  # def initialize
-  #   puts self.class.get('/creators/')
-  #   # @options = {query: {site: service, page: page}}
-  # end
 
-  def creators
-    puts self.class.get('/creators/')
+  def creators(artist)
+    @artist = artist
+    uri = "/creators/?name=#{artist}"
+    puts self.class.get(uri)
+    # puts self.class.get('/creators/?name=#{artist}')
   end
-
-  # def monet
-  #   self.class.get('/creators/?name=Monet', @options)
-  # end
-  # def creators
-  #   self.class.get('/creators/')
-  # end
 
 end
 
 cleveland_museum_of_art = Cma.new
-puts cleveland_museum_of_art.creators
+puts cleveland_museum_of_art.creators("monet")
 
 
