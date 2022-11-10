973b85175006e04d542f7543cca3d4037a61df84
metasignal <timcollinsdesign+metasignal@gmail.com>
2022-11-10 10:10:28 -0500

    got requests up and running! yessss

--- app.rb
+++ app.rb
@@ -1,5 +1,32 @@
 require 'httparty'
 
-response = HTTParty.get('http://api.stackexchange.com/2.2/questions?site=stackoverflow')
+response = HTTParty.get('https://openaccess-api.clevelandart.org/api/creators/?name=Monet')
 
-puts response.body, response.code, response.message, response.headers.inspect
+# puts response.body, response.code, response.message, response.headers.inspect
+
+class Cma
+  include HTTParty
+  base_uri 'https://openaccess-api.clevelandart.org/api'
+  
+  # def initialize
+  #   puts self.class.get('/creators/')
+  #   # @options = {query: {site: service, page: page}}
+  # end
+
+  def creators
+    puts self.class.get('/creators/')
+  end
+
+  # def monet
+  #   self.class.get('/creators/?name=Monet', @options)
+  # end
+  # def creators
+  #   self.class.get('/creators/')
+  # end
+
+end
+
+cleveland_museum_of_art = Cma.new
+puts cleveland_museum_of_art.creators
+
+
